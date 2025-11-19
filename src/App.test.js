28714ef7603fbacdf7e44189ecb26c466aab62bd
will wade <willwade@gmail.com>
2025-11-19 23:01:36 +0000

    update dependencies, migrate to vite

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders speech markdown logo", () => {
+  const { getByAltText } = render(<App />);
+  const logoElement = getByAltText(/Speech Markdown Logo/i);
+  expect(logoElement).toBeInTheDocument();
 });
---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  plugins: [react(), svgr()],
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  server: {
+    open: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    css: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["legacy-js-api", "import", "slash-div", "global-builtin"],
+      },
+    },
+  },
+});
